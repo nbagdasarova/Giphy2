38a519aea0dd23b8f55a7d6c9c227440dc342221
nbagdasarova <110225579+nbagdasarova@users.noreply.github.com>
2022-08-26 22:20:16 +0400

    Update main.js
    second commit

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
-import {Search} from './module/search.js'
-const giphyKey='aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB';
+import { Search } from "./module/search.js";
+const giphyKey = "aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB";
+
+const submitBtn = document.querySelector("#submit");
+const inputText = document.querySelector("#input-text");
+
+submitBtn.addEventListener("click", searchData);
+
+// fetch with given search value
 function searchGif(searchValue) {
-   return fetch (`https://api.giphy.com/v1/gifs/search?q=${searchValue}&limit=10&api_key=${giphyKey}&fmt=json`)
-.then(response=>response.json())
+  return fetch(
+    `https://api.giphy.com/v1/gifs/search?q=${searchValue}&limit=10&api_key=${giphyKey}&fmt=json`
+  ).then((response) => response.json());
 }
-
-const value='smile';
-console.log (searchGif(value));
-
-async function searchData (value) {
-    let result = await searchGif(value);
-    result.data.forEach(data => {
-        console.log (data.images.original.webp);
-        let img= new Search(result);
-        img.render();
-        });
+// fetch results
+async function searchData(value) {
+  var value = inputText.value;
+  let result = await searchGif(value);
+  result.data.forEach((data) => {
+    console.log(data.images.original.webp);
+    let img = new Search(result);
+    img.render();
+  });
 }
- console.log ('test',searchData(value));
