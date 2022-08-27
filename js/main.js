64f864ddec246512539cfe3a9127dd07ecc06c59
nbagdasarova <110225579+nbagdasarova@users.noreply.github.com>
2022-08-27 10:55:43 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ const giphyKey = "aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB";
 
 const submitBtn = document.querySelector("#submit");
 const inputText = document.querySelector("#input-text");
+const trendBtn = document.querySelector("#trend-btn");
 
 submitBtn.addEventListener("click", searchData);
+trendBtn.addEventListener("click", searchTrend);
 
 // fetch with given search value
 function searchGif(searchValue) {
@@ -16,6 +18,22 @@ function searchGif(searchValue) {
 async function searchData(value) {
   var value = inputText.value;
   let result = await searchGif(value);
+  result.data.forEach((data) => {
+    console.log(data.images.original.webp);
+    let img = new Search(result);
+    img.render();
+  });
+}
+
+// trenging gyphi
+function trendsGif() {
+  return fetch(
+    `https://api.giphy.com/v1/gifs/trending?limit=10&api_key=${giphyKey}&fmt=json`
+  ).then((response) => response.json());
+}
+
+async function searchTrend() {
+  let result = await trendsGif();
   result.data.forEach((data) => {
     console.log(data.images.original.webp);
     let img = new Search(result);
