c7c36bf9b7bd98b9255c04bae5582b2598dfd841
nbagdasarova <110225579+nbagdasarova@users.noreply.github.com>
2022-08-28 14:27:11 +0400

    Update array.js

--- js/module/array.js
+++ js/module/array.js
@@ -5,7 +5,7 @@ export class _BtnArray {
 
   getBtnArr() {
     let btnList = this.appJs.map((obj) => {
-      return ` <input type="button" value="${obj}" id="hot-buttons"/>`;
+      return ` <input type="button" value="${obj}" class="hot-buttons"/>`;
     });
     return btnList.join("");
   }
@@ -19,12 +19,4 @@ export class _BtnArray {
     this.setBtnArr("buttons", this.getBtnArr());
   }
 
-  getBtnValue() {
-    let elem = document.querySelectorAll("#hot-buttons");
-    elem.forEach((button) => {
-      button.addEventListener("click", () => {
-        const hot_button = button.value;
-      });
-    });
-  }
 }
