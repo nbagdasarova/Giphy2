30c21a9f819f8d1a606e7e6e5ea0db02014ca4d8
nbagdasarova <110225579+nbagdasarova@users.noreply.github.com>
2022-08-28 12:00:29 +0400

    Create array.js

---
+++ js/module/array.js
@@ -0,0 +1,30 @@
+export class BtnArray {
+  constructor(appJs) {
+    this.appJs = appJs;
+  }
+
+  getBtnArr() {
+    let btnList = this.appJs.map((obj) => {
+      return ` <input type="button" value="${obj}" id="hot-buttons"/>`;
+    });
+    return btnList.join("");
+  }
+
+  setBtnArr(id, value) {
+    let elem = document.getElementById(id);
+    elem.innerHTML = value;
+  }
+
+  render() {
+    this.setBtnArr("buttons", this.getBtnArr());
+  }
+
+  getBtnValue() {
+    let elem = document.querySelectorAll("#hot-buttons");
+    elem.forEach((button) => {
+      button.addEventListener("click", () => {
+        const hot_button = button.value;
+      });
+    });
+  }
+}
